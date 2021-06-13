5bd1edd48f8c052eab03227b5f2d025bf85f3917
Anindita <oishiroy99@gmail.com>
2021-06-13 13:32:26 +0600

    Lab3

--- Lab3/task 1.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <title> Problem 1</title>
-</head>
-<body>
-    <p> A quick brown <?php echo "<b>fox</b>";?> jumps over the lazy <?php echo "<b>dog</b>";?></p>
-	</body>
-	</html>
