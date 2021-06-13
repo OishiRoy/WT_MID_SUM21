e56e12ff34a042a7d6512b234ca7f4aca43e3849
Anindita <oishiroy99@gmail.com>
2021-06-13 13:05:02 +0600

    Lab3

---
+++ Lab3/task3.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Problem 3</title>
+</head>
+<body>
+<?php
+    /*$length = 15;
+    $width = 20;
+    $p = 2*( $length+$width ) ;
+    $area = $length*$width;
+    print $p;*/
+    
+    $length=15;
+    $width = 20;
+    $p = 2*($length+$width);
+    $area = $length*$width;
+    print $p;
+
+ 
+
+?>
+</body>
+</html>
